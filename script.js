283ef89f074d72af8a0653b387216e9e80ac147e
Renan Storrer <storrerrenan@gmail.com>
2022-07-13 16:29:14 -0300

    requisito 1, 2 e 3

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('entrar');
+
+function login() {
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('password').value;
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+}
+
+button.onclick = login;
